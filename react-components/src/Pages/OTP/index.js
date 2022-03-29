be0eeb9e5125c8b22ab62d7b031ecb0d1fbc06a2
Sujith Gunasekaran <mydevelopstar@gmail.com>
2022-03-29 17:14:45 +0530

    OTP Component updated

--- react-components/src/Pages/OTP/index.js
+++ react-components/src/Pages/OTP/index.js
@@ -1,6 +1,5 @@
-import React, { Fragment, useState, Suspense, lazy } from 'react';
+import React, { Fragment, useState, Suspense, lazy, useEffect, useRef } from 'react';
 import './style.css';
-
 
 const PageNavigation = lazy(() => import('../../Components/PageNavigation'));
 const PageHeader = lazy(() => import('../../Components/PageHeader'));
@@ -8,27 +7,88 @@ const PageHeader = lazy(() => import('../../Components/PageHeader'));
 
 const OTPPage = () => {
 
-    const [otpNumber, setOtpNumber] = useState(new Array(4).fill(''));
-    const [activeInput, setActiveInput] = useState(0);
+    // State
+    const [otpNumber, setOtpNumber] = useState([]);
+    const [verifiedOtp, setVerifiedOtp] = useState(null);
 
-    const handleOtpChange = (e, idx) => {
-        const { value } = e.target;
-        console.log(idx);
-        if (isNaN(value)) return;
-        setOtpNumber(prevNumber => {
-            let otpNumber = prevNumber.map((num, index) => idx === index ? value : num);
+    // ref
+    const constructOtpInputRef = useRef(null);
+
+    constructOtpInputRef.current = () => {
+        setOtpNumber(() => {
+            let otpNumber = [];
+            for (let i = 0; i < 4; i++) {
+                otpNumber.push({
+                    id: `${i}`,
+                    otp: ''
+                });
+            }
             return otpNumber;
         })
     }
 
-    const handleKeyChange = (e, idx) => {
-        const { nextElementSibling } = e.target;
-        if (e.keyCode !== 8) {
-            setActiveInput(idx + 1)
+    useEffect(() => {
+        constructOtpInputRef.current();
+    }, [])
+
+
+    const handleOtpChange = (e, otpInfo) => {
+        const { value } = e.target;
+        if (isNaN(value) || +value > 9) return;
+        setOtpNumber(prevNumber => {
+            let otpNumber = prevNumber.map((info) => info.id === otpInfo.id ? { ...info, otp: value } : info);
+            return otpNumber;
+        })
+    }
+
+    const removePrevValue = (targetElement) => {
+        const { id } = targetElement;
+        const currentIndex = id.split('-')[1];
+        setOtpNumber(prevNumber => {
+            let otpNumber = prevNumber.map((info) => info.id === currentIndex ? { ...info, otp: '' } : info);
+            return otpNumber;
+        })
+    }
+
+    const focusPrevInput = (targetElement) => {
+        const { id } = targetElement;
+        const currentIndex = id.split('-')[1];
+        if (+currentIndex === 0) return;
+        document.querySelector(`#otp-${currentIndex - 1}`).focus();
+    }
+
+    const handleKeyChange = (e) => {
+        if (e.keyCode === 8) {
+            const { target } = e;
+            e.preventDefault();
+            removePrevValue(target);
+            focusPrevInput(target);
         }
     }
 
-    console.log(otpNumber, activeInput);
+    const handleInputChange = (e) => {
+        const { nextSibling } = e.target;
+        if (nextSibling) {
+            nextSibling.focus();
+        }
+    }
+
+    const resetOtpNumber = () => {
+        constructOtpInputRef.current();
+        setVerifiedOtp(null);
+    }
+
+    const verifiyOtpNumber = () => {
+        let result = '';
+        otpNumber.forEach(info => {
+            result += info.otp;
+        })
+        if (result === '') {
+            setVerifiedOtp(null);
+            return;
+        };
+        setVerifiedOtp(result);
+    }
 
     return (
         <Fragment>
@@ -49,18 +109,26 @@ const OTPPage = () => {
                                 <div className='opt_heading'>Enter OTP send to you to verify the identity</div>
                                 <div className='opt_box_container'>
                                     {
-                                        otpNumber.map((num, index) => (
+                                        otpNumber.map((otpInfo) => (
                                             <input
-                                                key={index}
-                                                value={num}
-                                                focus={activeInput === index}
-                                                tabIndex={`${index + 1}`}
-                                                onChange={(e) => handleOtpChange(e, index)}
-                                                onKeyDown={(e) => handleKeyChange(e, index)}
+                                                id={`otp-${otpInfo.id}`}
+                                                key={otpInfo.id}
+                                                value={otpInfo.otp}
+                                                onChange={(e) => handleOtpChange(e, otpInfo)}
+                                                onKeyDown={(e) => handleKeyChange(e)}
+                                                onInput={(e) => handleInputChange(e)}
                                                 className='otp_input_box'
                                             />
                                         ))
                                     }
+                                </div>
+                                {
+                                    verifiedOtp &&
+                                    <div className='otp_form_verify'>Entered Otp - {verifiedOtp}</div>
+                                }
+                                <div className='otp_form_footer'>
+                                    <button className='otp_form_btn cancel' onClick={() => resetOtpNumber()}>Cancel</button>
+                                    <button className='otp_form_btn' onClick={() => verifiyOtpNumber()}>Verify</button>
                                 </div>
                             </div>
                         </div>
